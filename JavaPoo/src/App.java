f339703ce3704fa1124ff20fd064fdf486566392
andresbernal29 <bernalramireza68@gmail.com>
2024-09-07 13:44:33 -0500

    java metodos

--- JavaPoo/src/App.java
+++ JavaPoo/src/App.java
@@ -1,15 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Persona persona1 = new Persona();
-        persona1.nombre = "Raul";
+        persona1.nombre = "Andres";
         persona1.apellidos = "Alvares";
-        persona1.edad = 53;
-        
-        System.out.println(persona1.apellidos + ", " + persona1.nombre + " Tiene "+ persona1.edad + " Años.");
+        persona1.edad = 25;  
+        persona1.carrera.nombre = "musica";    
+
+        System.out.println(persona1.darNombreCompleto() + ", "+ " Tiene " + persona1.edad
+                + " Años y esta recibido de:." + persona1.carrera.nombre);
         
 
-    
-        
     }
 }
-
--- JavaPoo/src/Persona.java
+++ JavaPoo/src/Persona.java
@@ -2,7 +2,20 @@ public class Persona {
 String nombre;
 String apellidos;
 int edad;
+Carrera carrera;
+//contructor
+public Persona(){
+    carrera = new Carrera();
+}
 
+// Metodos (compportasmiento de un objeto)
+public String darNombreCompleto(){
+return apellidos + ", " + nombre;
+}
+public String enviarSaludo (String saludado){
+    if (edad > 40)return "Buenos días querido " + saludado;      
+    return "Hola, Como estas? " + saludado + "?";
+}
 }
 
 
