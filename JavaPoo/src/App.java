8b871d911b1d4d0cd86428a39de80e7cd9828144
andresbernal29 <bernalramireza68@gmail.com>
2024-09-07 12:19:49 -0500

    nuevoPro

---
+++ JavaPoo/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona persona1 = new Persona();
+        persona1.nombre = "Raul";
+        persona1.apellidos = "Alvares";
+        persona1.edad = 53;
+        System.out.println(persona1.apellido + ", " + persona1.nombre + "Tiene "+ persona1.edad + "Años.");
+
+    
+        
+    }
+}
