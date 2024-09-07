b00acd9f068f0f2f0b4fca731201670aa848066e
Edgar Bernal <99697813+andresbernal29@users.noreply.github.com>
2024-09-07 12:40:53 -0500

    Add files via upload

---
+++ JavaPoo/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona persona1 = new Persona();
+        persona1.nombre = "Raul";
+        persona1.apellidos = "Alvares";
+        persona1.edad = 53;
+        System.out.println(persona1.apellidos + ", " + persona1.nombre + " Tiene "+ persona1.edad + " Años.");
+        
+
+    
+        
+    }
+}
